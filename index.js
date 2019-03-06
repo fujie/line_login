0a6adcd387bc0862651db32f972f4201aad90185
fujie <pharaoh@eidentity.jp>
2019-03-07 01:41:50 +0900

    use request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const https = require('https');
+const request = require('request');
 const querystring = require('querystring');
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -30,45 +31,30 @@ app.get('/', function (req, res) {
     res.redirect(destination);
 })
 
-app.get('/cb', function (req, res) {
+app.get('/cb', function (req, resp) {
     // verify state
 
     // post authorization code and exchange to access_token, id_token
-    let postData = querystring.stringify({
-        grant_type: 'authorization_code',
-        code: req.query.code,
-        redirect_uri: redirect_uri,
-        client_id: client_id,
-        client_secret: client_secret
-      });
-    let options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+    request.post(
+        token_endpoint,
+        {form:
+        {
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: redirect_uri,
+            client_id: client_id,
+            client_secret: client_secret
+        }},
+        function(err, res, body){
+            if (!err && res.statusCode == 200) {
+                console.log(body);
+                resp.json(body);
+            } else {
+                resp.send("error");
+                console.log(body);
+            }               
         }
-    };      
-    let httpreq = https.request(token_endpoint, options, function (httpres) {
-        let result = '';
-        httpres.on('data', function (chunk) {
-          result += chunk;
-        });
-        httpres.on('end', function () {
-          console.log(result);
-          res.json(result);
-        });
-        httpres.on('error', function (err) {
-          console.log(err);
-        })
-      });
-       
-      // req error
-      httpreq.on('error', function (err) {
-        console.log(err);
-      });
-       
-      //send request witht the postData form
-      httpreq.write(postData);
-      httpreq.end();
+    )
 })
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));

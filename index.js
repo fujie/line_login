3d0a5d6a0d0e2eeea01e95b5b9ca4df3fe893c41
fujie <pharaoh@eidentity.jp>
2019-03-13 22:56:48 +0900

    userinfo

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const request = require('request');
 const uuidv4 = require('uuid/v4');
 const session = require('express-session');
 const random = require('crypto');
+const bodyParser = require('body-parser');
+const jwt_decode = require('jwt-decode');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const authorization_endpoint = 'https://access.line.me/oauth2/v2.1/authorize';
 const token_endpoint = 'https://api.line.me/oauth2/v2.1/token';
 const profile_endpoint = 'https://api.line.me/v2/profile';
-//const client_id = '1516319320';
-const client_id = '1537859534';
-//const client_secret = 'fd1a749be0c9d7eb21faf4810cdcfd4a';
-const client_secret = 'fdb329ce3924dd50c043fdade69595f3';
+const client_id = '1516319320';
+const client_secret = 'fd1a749be0c9d7eb21faf4810cdcfd4a';
 const redirect_uri = 'http://localhost:3000/cb';
 
 app.use(session({
@@ -23,6 +23,7 @@ app.use(session({
         maxAge: 30*60*1000
     }
 }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.send('<html><body><form method="get" action="/login"><button type="submit">login</button></form></body></html>');
@@ -49,66 +50,88 @@ app.get('/login', function (req, res) {
     res.redirect(destination);
 })
 
-app.get('/cb_', function (req, res) {
-    res.send('<html><body>'
-            + '<form method="post" action="' + token_endpoint + '">'
-            + '<table><tr><th>grant_type</th><td><input type="text" name="grant_type" value="authorization_code"></td></tr>'
-            + '<tr><th>code</th><td><input type="text" name="code" value="' + req.query.code + '"></td></tr>'
-            + '<tr><th>redirect_uri</th><td><input type="text" name="redirect_uri" value="' + redirect_uri + '"></td></tr>'
-            + '<tr><th>client_id</th><td><input type="text" name="client_id" value="' + client_id + '"></td></tr>'
-            + '<tr><th>client_secret</th><td><input type="text" name="client_secret" value="' + client_secret + '"></td></tr>'
-            + '</table><button type="submit">Exchange code to token</button><br>'
-            + '</form></body></html>'
-         );
-})
-app.get('/cb', function (req, resp) {
-    // verify state
+app.get('/cb', function (req, res) {
     if(req.query.state != req.session.state){
         resp.send("state unmatch error");
         console.log('state unmatch!');
     } else {
-        // post authorization code and exchange to access_token, id_token
-        request.post(
-            token_endpoint,
-            {form:
-            {
-                grant_type: 'authorization_code',
-                code: req.query.code,
-                redirect_uri: redirect_uri,
-                client_id: client_id,
-                client_secret: client_secret
-            }},
-            function(err, res, body){
-                if (!err && res.statusCode == 200) {
-                    console.log(body);
-//                    resp.json(body);
-                    var jsonBody = JSON.parse(body);
-//                    resp.send(jsonBody.access_token);
-                    // get nonce from id_token and verify
-                    
-                    request.get(
-                        profile_endpoint,
-                        {
-                            headers: {
-                                'Authorization': 'Bearer ' + jsonBody.access_token
-                            }
-                        },
-                        function(err, res, body){
-                            if (!err && res.statusCode == 200) {
-                                console.log(body);
-                                resp.send(body);
-                            } else {
-                                console.log("error" + res.statusCode.toString());
-                            }
-                        }
-                    )
-                } else {
-                    resp.send("error");
-                    console.log(body);
-                }               
-            }
-        )
+        res.send('<html><body>'
+                + '<form method="post" action="/token">'
+                + '<table><tr><th>grant_type</th><td><input type="text" name="grant_type" size="100" value="authorization_code"></td></tr>'
+                + '<tr><th>code</th><td><input type="text" name="code" size="100" value="' + req.query.code + '"></td></tr>'
+                + '<tr><th>redirect_uri</th><td><input type="text" name="redirect_uri" size="100" value="' + redirect_uri + '"></td></tr>'
+                + '<tr><th>client_id</th><td><input type="text" name="client_id" size="100" value="' + client_id + '"></td></tr>'
+                + '<tr><th>client_secret</th><td><input type="text" name="client_secret" size="100" value="' + client_secret + '"></td></tr>'
+                + '</table><button type="submit">Exchange code to token</button><br>'
+                + '</form></body></html>'
+            );
     }
+})
+
+app.post('/token', function (req, resp) {
+    // post authorization code and exchange to access_token, id_token
+    request.post(
+        token_endpoint,
+        {form:
+        {
+            grant_type: 'authorization_code',
+            code: req.body.code,
+            redirect_uri: redirect_uri,
+            client_id: client_id,
+            client_secret: client_secret
+        }},
+        function(err, res, body){
+            if (!err && res.statusCode == 200) {
+                var jsonBody = JSON.parse(body);
+                // verify nonce in jwt
+                var id_token = jwt_decode(jsonBody.id_token);
+                if (id_token.nonce != req.session.nonce){
+                    resp.send("nonce unmatch error");
+                    console.log('nonce unmatch!');            
+                } else {
+                    resp.send('<html><body>'
+                        + '<form method="post" action="/userInfo">'
+                        + '<table><tr><th>access_token</th><td><input type="text" name="access_token" size="100" value="' + jsonBody.access_token + '"></td></tr>'
+                        + '<tr><th>token_type</th><td><input type="text" name="token_type" size="100" value="' + jsonBody.token_type + '"></td></tr>'
+                        + '<tr><th>refresh_token</th><td><input type="text" name="refresh_token" size="100" value="' + jsonBody.refresh_token + '"></td></tr>'
+                        + '<tr><th>expires_in</th><td><input type="text" name="expires_in" size="100" value="' + jsonBody.expires_in + '"></td></tr>'
+                        + '<tr><th>scope</th><td><input type="text" name="scope" size="100" value="' + jsonBody.scope + '"></td></tr>'
+                        + '<tr><th><a href="https://jwt.ms#id_token=' + jsonBody.id_token + '" target="_blank">id_token</a></th><td><input type="text" name="id_token" size="100" value="' + jsonBody.id_token + '"></td></tr>'
+                        + '</table><button type="submit">get userInfo</button><br>'
+                        + '</form></body></html>'
+                    );
+                }
+            } else {
+                resp.send("error");
+                console.log(body);
+            }               
+        }
+    )
+})
+
+app.post('/userInfo', function (req, resp) {
+    request.get(
+        profile_endpoint,
+        {
+            headers: {
+                'Authorization': 'Bearer ' + req.body.access_token
+            }
+        },
+        function(err, res, body){
+            if (!err && res.statusCode == 200) {
+                var jsonBody = JSON.parse(body);
+                resp.send('<html><body>'
+                    + '<table border="1"><tr><th>userId</th><td>' + jsonBody.userId + '</td></tr>'
+                    + '<tr><th>displayName</th><td>' + jsonBody.displayName + '</td></tr>'
+                    + '<tr><th>pictureUrl</th><td>' + jsonBody.pictureUrl + '</td></tr>'
+                    + '</table>'
+                    + '</body></html>'
+                );
+            } else {
+                console.log("error" + res.statusCode.toString());
+            }
+        }
+    )
 })
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));

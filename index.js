bbffaf9335414e0926f97dec16d8c5210a452fb1
fujie <pharaoh@eidentity.jp>
2019-03-16 14:58:44 +0900

    verfy id_token signature

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const uuidv4 = require('uuid/v4');
 const session = require('express-session');
 const random = require('crypto');
 const bodyParser = require('body-parser');
-const jwt_decode = require('jwt-decode');
+const jsonwebtoken = require('jsonwebtoken');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const authorization_endpoint = 'https://access.line.me/oauth2/v2.1/authorize';
@@ -104,24 +104,30 @@ app.post('/token', function (req, res) {
         function(e, r, body){
             if (!e && r.statusCode == 200) {
                 var jsonBody = JSON.parse(body);
-                // verify nonce in jwt
-                var id_token = jwt_decode(jsonBody.id_token);
-                if (id_token.nonce != req.session.nonce){
-                    res.send("nonce unmatch error");
-                    console.log('nonce unmatch!');            
-                } else {
-                    // show form
-                    res.send('<html><body>'
-                        + '<form method="post" action="/userInfo">'
-                        + '<table><tr><th>access_token</th><td><input type="text" name="access_token" size="100" value="' + jsonBody.access_token + '"></td></tr>'
-                        + '<tr><th>token_type</th><td><input type="text" name="token_type" size="100" value="' + jsonBody.token_type + '"></td></tr>'
-                        + '<tr><th>refresh_token</th><td><input type="text" name="refresh_token" size="100" value="' + jsonBody.refresh_token + '"></td></tr>'
-                        + '<tr><th>expires_in</th><td><input type="text" name="expires_in" size="100" value="' + jsonBody.expires_in + '"></td></tr>'
-                        + '<tr><th>scope</th><td><input type="text" name="scope" size="100" value="' + jsonBody.scope + '"></td></tr>'
-                        + '<tr><th><a href="https://jwt.ms#id_token=' + jsonBody.id_token + '" target="_blank">id_token</a></th><td><input type="text" name="id_token" size="100" value="' + jsonBody.id_token + '"></td></tr>'
-                        + '</table><button type="submit">get userInfo</button><br>'
-                        + '</form></body></html>'
-                    );
+                // verify jwt signature
+                try {
+                    var id_token = jsonwebtoken.verify(jsonBody.id_token, client_secret);
+                    // verify nonce in jwt
+                    if (id_token.nonce != req.session.nonce){
+                        res.send("nonce unmatch error");
+                        console.log('nonce unmatch!');            
+                    } else {
+                        // show form
+                        res.send('<html><body>'
+                            + '<form method="post" action="/userInfo">'
+                            + '<table><tr><th>access_token</th><td><input type="text" name="access_token" size="100" value="' + jsonBody.access_token + '"></td></tr>'
+                            + '<tr><th>token_type</th><td><input type="text" name="token_type" size="100" value="' + jsonBody.token_type + '"></td></tr>'
+                            + '<tr><th>refresh_token</th><td><input type="text" name="refresh_token" size="100" value="' + jsonBody.refresh_token + '"></td></tr>'
+                            + '<tr><th>expires_in</th><td><input type="text" name="expires_in" size="100" value="' + jsonBody.expires_in + '"></td></tr>'
+                            + '<tr><th>scope</th><td><input type="text" name="scope" size="100" value="' + jsonBody.scope + '"></td></tr>'
+                            + '<tr><th><a href="https://jwt.ms#id_token=' + jsonBody.id_token + '" target="_blank">id_token</a></th><td><input type="text" name="id_token" size="100" value="' + jsonBody.id_token + '"></td></tr>'
+                            + '</table><button type="submit">get userInfo</button><br>'
+                            + '</form></body></html>'
+                        );
+                    }
+                } catch(err) {
+                    res.send("error");
+                    console.log(err.toString());
                 }
             } else {
                 res.send("error");
